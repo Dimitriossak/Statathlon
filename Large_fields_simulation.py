aab41a9d12036160c1f9be56088847e1e6b866db
Dimitriossak <32390032+Dimitriossak@users.noreply.github.com>
2017-09-29 15:06:18 +0300

    Update Large_fields_simulation.py

--- Large_fields_simulation.py
+++ Large_fields_simulation.py
@@ -13,9 +13,9 @@ def sim():
 
 
 total_points = []
-for i in range(1000000):  # 1000000 fores kanoume simulate to prwtathlima
+for i in range(1000000):  # number of season simulations
     season_scores = []
-    for j in range(31):  # 30 agwnes se large gipedo
+    for j in range(31):  # number of matches in large field
         season = sim()
         season_scores.append(season)
     total_points.append(sum(season_scores))
